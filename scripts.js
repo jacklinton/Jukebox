3f38cf4c47a41317f567a8114c4f6b5fa08a7b0b
Jack Linton <jacklinton@mac.com>
2017-01-19 17:32:14 -0500

    Made a track appear on load and tried to make it a button where you clock on it and then it becomes playable. For some reason the file cannot be found using the mouse-click event. There's some kind of scope issue I can't resolve.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,25 @@
 function getAudioPlayer(){
 	document.getElementById("mainBody").innerHTML = "<audio type='audio/mp3' controls id='audioPlayer'></audio>"
 	var audioPlayer = document.getElementById("audioPlayer")
-	audioPlayer.src = "../Jukebox/Assets/tunes/2. Loser.mp3"
+	audioPlayer.src = "../Jukebox/Assets/tunes/1. Truckin.mp3"
+	audioFile1 = new AddAudioFile("1. Truckin.mp3", "../Jukebox/Assets/tunes/1. Truckin.mp3")
 }
 
+function audioFile() {
+
+}
+
+var trackCounter = 0
+function AddAudioFile(trackname, tracklocation) {
+	trackCounter += 1
+	this.trackname = trackname
+	this.tracklocation = tracklocation
+	var newDiv = document.createElement("div")
+	newDiv.setAttribute("id", "track"+ trackCounter) 
+	newDiv.classList.add("audioFile")
+	document.getElementById("mainBody").appendChild(newDiv)
+	function makeClickable(){document.getElementById("track" + trackCounter).addEventListener("click", audioPlayer.src = this.tracklocation)}
+	newDiv.innerHTML = "<h1>" + this.trackname + "</h1>"
+	makeClickable(newDiv)
+}
 
