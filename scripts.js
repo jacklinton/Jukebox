d7e5d9820a19ccfde32c58cc006b272db11f6cd0
Jack Linton <jacklinton@mac.com>
2017-01-19 16:04:58 -0500

    Added audio files. Created Assets folder. Started audio player in script and got it linked up to a single audio file.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
-document.getElementById("mainBody").innerHTML = "<h1>Hello World!</h1>"
+function getAudioPlayer(){
+	document.getElementById("mainBody").innerHTML = "<audio type='audio/mp3' controls id='audioPlayer'></audio>"
+	var audioPlayer = document.getElementById("audioPlayer")
+	audioPlayer.src = "../Jukebox/Assets/tunes/2. Loser.mp3"
+}
+
+
