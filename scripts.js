71d2088417a6794aaf01429c52a618de47dc0189
Jack Linton <jacklinton@mac.com>
2017-01-23 15:40:01 -0500

    Changed Button Styles, fixed some formatting, made the playlist functional, added download button.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,16 @@
-function getAudioPlayer(){
-	document.getElementById("mainBody").innerHTML = "<audio type='audio/mp3' controls id='audioPlayer' class='audioElement'></audio>"
-	var audioPlayer = document.getElementById("audioPlayer")
+var audioFileList = []
+var trackCounter = 0
 
+
+function Jukebox(){
+	getAudioPlayer()
+	nextTrack()
+
+	function getAudioPlayer(){
+
+	document.getElementById("playerBody").innerHTML = "<audio type='audio/mp3' controls id='audioPlayer' class='audioElement'></audio>"
+	this.audioPlayer = document.getElementById("audioPlayer")
+	
 	newAudioFile("1. Truckin", "https://www.archive.org/download/gd1971-04-25.sbd.matera.113038.sbeok.flac16/gd71-04-25d1t01.mp3")
 	newAudioFile("2. Loser", "https://www.archive.org/download/gd1971-04-25.sbd.matera.113038.sbeok.flac16/gd71-04-25d1t02.mp3")
 	newAudioFile("3. Hard To Handle", "https://www.archive.org/download/gd1971-04-25.sbd.matera.113038.sbeok.flac16/gd71-04-25d1t03.mp3")
@@ -13,85 +22,111 @@ function getAudioPlayer(){
 	newAudioFile("9. China Cat Sunflower ->", "https://www.archive.org/download/gd1971-04-25.sbd.matera.113038.sbeok.flac16/gd71-04-25d1t09.mp3")
 	newAudioFile("10. I Know You Rider", "https://www.archive.org/download/gd1971-04-25.sbd.matera.113038.sbeok.flac16/gd71-04-25d1t10.mp3")
 	newAudioFile("11. Casey Jones", "https://www.archive.org/download/gd1971-04-25.sbd.matera.113038.sbeok.flac16/gd71-04-25d1t11.mp3")
-	nextTrack()
 
-	var newTrackButton = document.createElement("div")
-	newTrackButton.setAttribute("id", "newTrackButton")
-	newTrackButton.classList.add("addButton")
-	document.getElementById("mainBody").appendChild(newTrackButton)
-	newTrackButton.innerHTML = "<h1>Add a Track</h1>"
-	newTrackButton.addEventListener("click", function(){
-		newTrack = prompt("Please enter the name of the track you wish to add.")
-		newLocation = prompt("Paste the full URL of the track here.")
-		newAudioFile(newTrack, newLocation)
-	})
-
-	var pauseButton = document.createElement("div")
-	pauseButton.setAttribute("id", "pauseButton")
-	pauseButton.classList.add("controlButton")
-	document.getElementById("mainBody").appendChild(pauseButton)
-	pauseButton.innerHTML = "<h1>Pause</h1>"
-	pauseButton.addEventListener("click", function(){
-		audioPlayer.pause()
-	})
-
-	var playButton = document.createElement("div")
-	playButton.setAttribute("id", "playButton")
-	playButton.classList.add("controlButton")
-	document.getElementById("mainBody").appendChild(playButton)
-	playButton.innerHTML = "<h1>Play</h1>"
-	playButton.addEventListener("click", function(){
-		audioPlayer.play()
-	})
-
-
-}
-
-
-
-
-var audioFileList = []
-
-var trackCounter = 0
-function AddAudioFile(trackname, tracklocation) {
-
-	trackCounter += 1
 	
-	this.trackname = trackname
-	this.tracklocation = tracklocation
+	this.newTrackButton = document.createElement("div")
+		newTrackButton.setAttribute("id", "newTrackButton")
+		newTrackButton.setAttribute("class", "btn btn-primary")
+		document.getElementById("playerBody").appendChild(newTrackButton)
+		newTrackButton.innerHTML = "<i class='glyphicon glyphicon-plus-sign'></i>"
+		newTrackButton.addEventListener("click", function(){
+			newTrack = prompt("Please enter the name of the track you wish to add.")
+			newLocation = prompt("Paste the full URL of the track here.")
+			newAudioFile(newTrack, newLocation)
+		})
 	
-	var newDiv = document.createElement("div")
-	newDiv.setAttribute("id", "track"+ trackCounter) 
-	trackID = newDiv.getAttribute("id")
-	newDiv.classList.add("audioFile")
-	document.getElementById("mainBody").appendChild(newDiv)
-	newDiv.innerHTML = "<h4>" + this.trackname + "</h4>"
+	this.pauseButton = document.createElement("div")
+		pauseButton.setAttribute("id", "pauseButton")
+		pauseButton.setAttribute("class", "btn btn-primary")
+		document.getElementById("playerBody").appendChild(pauseButton)
+		pauseButton.innerHTML = "<i class= 'glyphicon glyphicon-pause'></i>"
+		pauseButton.addEventListener("click", function(){
+			audioPlayer.pause()
+		})
 
-	makeClickable(this.tracklocation, this.trackID)
-	
-	function makeClickable(place, trackID){
-		document.getElementById("track"+trackCounter).addEventListener("click", function(){
-			audioPlayer.src = place
+	this.playButton = document.createElement("div")
+		playButton.setAttribute("id", "playButton")
+		playButton.setAttribute("class", "btn btn-primary")
+		document.getElementById("playerBody").appendChild(playButton)
+		playButton.innerHTML = "<i class='glyphicon glyphicon-play-circle'</i>"
+		playButton.addEventListener("click", function(){
 			audioPlayer.play()
 		})
 
+	this.downloadButton = document.createElement("div")
+		downloadButton.setAttribute("id", "downloadButton")
+		downloadButton.setAttribute("class", "btn btn-primary")
+		document.getElementById("playerBody").appendChild(downloadButton)
+		downloadButton.innerHTML = "<i class='glyphicon glyphicon-download-alt'</i>"
+		downloadButton.addEventListener("click", function(){
+			audioPlayer.download()
+		})
 	}
-}
 
-function newAudioFile (trackname, tracklocation){
-	this.audioFile = new AddAudioFile(trackname, tracklocation)
-	audioFileList.push(this.audioFile)
-}
 
-function getIndex(){
-	index = audioFileList.findIndex(i => i.tracklocation == audioPlayer.src)
-	return index
-}
-			function nextTrack(){
-				document.getElementById("audioPlayer").addEventListener("ended", function(){
-					nextIndex = getIndex()
-					audioPlayer.src = audioFileList[nextIndex += 1].tracklocation
-					audioPlayer.play()
-				})
-			}
 
+
+
+
+	
+
+	
+	function AddAudioFile(trackname, tracklocation) {
+
+		trackCounter += 1
+	
+		this.trackname = trackname
+		this.tracklocation = tracklocation
+	
+		var newDiv = document.createElement("div")
+		newDiv.setAttribute("id", "track"+ trackCounter) 
+		trackID = newDiv.getAttribute("id")
+		newDiv.classList.add("audioFile")
+		document.getElementById("playList").appendChild(newDiv)
+		newDiv.innerHTML = "<h4>" + this.trackname + "</h4>"
+
+		makeClickable(this.tracklocation, this.trackID)
+	
+		function makeClickable(place, trackID){
+			document.getElementById("track"+trackCounter).addEventListener("click", function(){
+				this.index = audioFileList.findIndex(i => i.tracklocation == place)
+				this.prevTrack = audioFileList.findIndex(i => i.tracklocation == audioPlayer.src)
+				if (this.prevTrack != -1) {
+					document.getElementById("track"+(this.prevTrack+1)).classList.remove("currentTrack")
+				}
+				document.getElementById("track"+(this.index+1)).classList.add("currentTrack")
+				audioPlayer.src = place
+				audioPlayer.play()
+			})
+
+		}
+	}
+
+	function newAudioFile (trackname, tracklocation){
+		this.audioFile = new AddAudioFile(trackname, tracklocation)
+		audioFileList.push(this.audioFile)
+	}
+
+	function getIndex(){
+		this.index = audioFileList.findIndex(i => i.tracklocation == audioPlayer.src)
+		return index
+	}
+
+	function nextTrack(){
+		document.getElementById("audioPlayer").addEventListener("ended", function(){
+		nextIndex = getIndex()	
+		if (nextIndex < (audioFileList.length-1)) {
+			audioPlayer.src = audioFileList[nextIndex += 1].tracklocation
+			document.getElementById("track" + (nextIndex += 1)).classList.remove("currentTrack")
+			document.getElementById("track" + (nextIndex += 2)).classList.add("currentTrack")
+		}
+		else {
+			audioPlayer.src = audioFileList[0].tracklocation
+			document.getElementById("track" + (nextIndex += 1)).classList.remove("currentTrack")
+			document.getElementById("track1").classList.add("currentTrack")
+		}
+		audioPlayer.play()
+		})
+	}
+
+}
